deb146285bd6bc18fc3a9bd9af81deca9c997d19
Svetlana Khlystova <khlystova_sv@bk.ru>
2024-12-20 16:32:39 +0300

    Change to ParameterizedTest

--- src/test/java/ru/netology/FreelanceCalculator/services/FreelanceServiceTest.java
+++ src/test/java/ru/netology/FreelanceCalculator/services/FreelanceServiceTest.java
@@ -2,28 +2,15 @@ package ru.netology.FreelanceCalculator.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class FreelanceServiceTest {
-    @Test
-    void shouldCalculateForExample1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/validdata.csv")
+    public void shouldCalculateForValidData(int expected, int income, int expense, int threshold) {
         FreelanceService service = new FreelanceService();
-        int income = 10_000;
-        int expense = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
-
-        int actual = service.calculate(income, expense, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void shouldCalculateForExample2() {
-        FreelanceService service = new FreelanceService();
-        int income = 100_000;
-        int expense = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
 
         int actual = service.calculate(income, expense, threshold);
 
